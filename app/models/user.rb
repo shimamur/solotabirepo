f665a8fcf95bada5a0f400e92312dc233d91009c
Ubuntu <ubuntu@ip-172-31-2-83.us-east-2.compute.internal>
2021-01-30 13:09:54 +0000

    add session test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remeber_token
   before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
@@ -17,6 +18,33 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 }
     
+ class << self
+　  
+　  def new_token
+　    SecureRandom.urlsafe_base64
+　  end
+    
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+    def authenticated?(remember_token)
+      return false if remember_digest.nill?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+    
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
+    
+ end 
     
 private
     def downcase_email
--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,8 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "UsersLogins", type: :request do
+  include SessionsHelper
   
   let(:user){create(:user)}
+  
+  def post_valid_information(remember_me = 0)
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: user.password,
+        remember_me: remember_me
+      }
+    }
+  end
+  
+  it "does not log out twice" do
+    get login_path
+    post_valid_information(0)
+    expect(is_logged_in?).to be_truthy
+    follow_redirect!
+    expect(request.fullpath).to eq '/users/1'
+    delete logout_path
+    expect(is_logged_in?).to be_falsey
+    follow_redirect!
+    expect(request.fullpath).to eq '/'
+    delete logout_path
+    follow_redirect!
+    expect(request.fullpath).to eq '/'
+  end
+
+  it "succeeds remember_token because of check remember_me" do
+    get login_path
+    post_valid_information(1)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).not_to be_nil
+  end
+
+  it "has no remember_token because of check remember_me" do
+    get login_path
+    post_valid_information(0)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).to be_nil
+  end
+
+  it "has no remember_token when users logged out and logged in" do
+    get login_path
+    post_valid_information(1)
+    expect(is_logged_in?).to be_truthy
+    expect(cookies[:remember_token]).not_to be_empty
+    delete logout_path
+    expect(is_logged_in?).to be_falsey
+    expect(cookies[:remember_token]).to be_empty
+  end
+    
   
   describe "GET /login" do
     context "invalid information" do
