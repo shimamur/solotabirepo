215a17b1378769915c5a6f166815e35d0ecc5d00
Ubuntu <ubuntu@ip-172-31-2-83.us-east-2.compute.internal>
2021-02-17 02:30:08 +0000

    add about page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@ User.create!(name:  "そろだ　たびお",
              email: "example@example.org",
              password:              "foobar",
              password_confirmation: "foobar")
+             
+users = User.order(:created_at).take(1)
+10.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(content: content) }
+end
